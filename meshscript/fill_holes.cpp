188b088fff2c6a6475752f33532d8d1e03149aef
janmaesgit <jan.maes@outlook.com>
2020-11-18 09:09:30 +0100

    jtk update + refactoring

--- meshscript/fill_holes.cpp
+++ meshscript/fill_holes.cpp
@@ -17,6 +17,72 @@ fill_hole_minimal_surface_parameters::fill_hole_minimal_surface_parameters()
   number_of_rings = 12;
   }
 
+
+namespace
+  {
+
+  bool edge_swap(uint32_t v, uint32_t v2, std::vector<jtk::vec3<uint32_t>>& triangles, jtk::mutable_adjacency_list& adj_list)
+    {
+    auto tria = jtk::triangle_indices_from_edge(v, v2, adj_list);
+    if (tria.size() != 2)
+      return false;
+
+    auto t1 = triangles[tria[0]];
+    auto t2 = triangles[tria[1]];
+    int t1v = 0;
+    while (t1[t1v] == v || t1[t1v] == v2)
+      ++t1v;
+    int t2v = 0;
+    while (t2[t2v] == v || t2[t2v] == v2)
+      ++t2v;
+
+    int vpos = 0;
+    while (t1[vpos] != v)
+      ++vpos;
+    int v2pos = 0;
+    while (t1[v2pos] != v2)
+      ++v2pos;
+
+    if ((v2pos + 1) % 3 == vpos)
+      {
+      std::swap(tria[0], tria[1]);
+      std::swap(t1, t2);
+      std::swap(t1v, t2v);
+      }
+
+    uint32_t T1v = t1[t1v];
+    uint32_t T2v = t2[t2v];
+
+    if (!jtk::triangle_indices_from_edge(T1v, T2v, adj_list).empty())
+      return false;
+
+    adj_list.remove_triangle_from_vertex(v2, tria[0]);
+    adj_list.remove_triangle_from_vertex(v, tria[1]);
+
+    t1[0] = T1v;
+    t1[1] = v;
+    t1[2] = T2v;
+
+    t2[0] = T1v;
+    t2[1] = T2v;
+    t2[2] = v2;
+
+    triangles[tria[0]] = t1;
+    triangles[tria[1]] = t2;
+
+    adj_list.add_triangle_to_vertex(T1v, tria[1]);
+    adj_list.add_triangle_to_vertex(T2v, tria[0]);
+
+    assert(jtk::triangle_indices_from_edge(T1v, T2v, adj_list).size() == 2);
+    assert(jtk::triangle_indices_from_edge(T1v, v2, adj_list).size() == 2);
+    assert(jtk::triangle_indices_from_edge(v2, T2v, adj_list).size() == 2);
+    assert(jtk::triangle_indices_from_edge(T1v, v, adj_list).size() == 2);
+    assert(jtk::triangle_indices_from_edge(v, T2v, adj_list).size() == 2);
+    return true;
+    }
+
+  }
+
 void fill_hole_minimal_surface(std::vector<jtk::vec3<uint32_t>>& triangles, std::vector<jtk::vec3<float>>& vertices, const std::vector<uint32_t>& hole, const fill_hole_minimal_surface_parameters& params)
   {
   int s = params.number_of_rings;
@@ -62,7 +128,7 @@ void fill_hole_minimal_surface(std::vector<jtk::vec3<uint32_t>>& triangles, std:
     }
 
   jtk::mutable_adjacency_list adj_list((uint32_t)vertices.size(), triangles.data(), (uint32_t)triangles.size());
-  for (int iter = 0; iter < 200; ++iter)
+  for (int iter = 0; iter < 2000; ++iter)
     {
     //first laplace fairing
     std::vector<jtk::vec3<float>> vert(vertices.begin() + center_id, vertices.end());
@@ -87,7 +153,7 @@ void fill_hole_minimal_surface(std::vector<jtk::vec3<uint32_t>>& triangles, std:
         float tria2_area = jtk::length(jtk::cross(get_vertex(triangles[tria[1]][1]) - get_vertex(triangles[tria[1]][0]), get_vertex(triangles[tria[1]][2]) - get_vertex(triangles[tria[1]][0])));
 
         float weight = tria1_area + tria2_area;
-
+        //weight = 1.f;
         umbrella = umbrella + weight * get_vertex(v2);
 
         total_area += weight;
@@ -128,53 +194,7 @@ void fill_hole_minimal_surface(std::vector<jtk::vec3<uint32_t>>& triangles, std:
 
             if (tria1_area_swapped + tria2_area_swapped < tria1_area + tria2_area)
               {
-              
-              
-              int vpos = 0;
-              while (t1[vpos] != v)
-                ++vpos;
-              int v2pos = 0;
-              while (t1[v2pos] != v2)
-                ++v2pos;
-
-              if ((v2pos + 1) % 3 == vpos)
-                {
-                std::swap(tria[0], tria[1]);
-                std::swap(t1, t2);
-                std::swap(t1v, t2v);
-                }                          
-
-              uint32_t T1v = t1[t1v];
-              uint32_t T2v = t2[t2v];
-
-              if (!jtk::triangle_indices_from_edge(T1v, T2v, adj_list).empty())
-                continue;
-
-              edge_swapped = true;
-              adj_list.remove_triangle_from_vertex(v2, tria[0]);
-              adj_list.remove_triangle_from_vertex(v, tria[1]);
-
-
-              t1[0] = T1v;
-              t1[1] = v;
-              t1[2] = T2v;
-
-              t2[0] = T1v;
-              t2[1] = T2v;
-              t2[2] = v2;
-
-              triangles[tria[0]] = t1;
-              triangles[tria[1]] = t2;
-
-              adj_list.add_triangle_to_vertex(T1v, tria[1]);
-              adj_list.add_triangle_to_vertex(T2v, tria[0]);
-
-              assert(jtk::triangle_indices_from_edge(T1v, T2v, adj_list).size() == 2);
-              assert(jtk::triangle_indices_from_edge(T1v, v2, adj_list).size() == 2);
-              assert(jtk::triangle_indices_from_edge(v2, T2v, adj_list).size() == 2);
-              assert(jtk::triangle_indices_from_edge(T1v, v, adj_list).size() == 2);
-              assert(jtk::triangle_indices_from_edge(v, T2v, adj_list).size() == 2);
-
+              edge_swapped = edge_swap(v, v2, triangles, adj_list);              
               }
 
             }
